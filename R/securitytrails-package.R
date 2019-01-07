441619ee774ba64066892b759eb9641646e0c3d4
hrbrmstr <bob@rud.is>
2019-01-07 14:49:43 -0500

    R package repo initialization complete

---
+++ R/securitytrails-package.R
@@ -0,0 +1,12 @@
+#' ...
+#' 
+#' - URL: <https://gitlab.com/hrbrmstr/securitytrails>
+#' - BugReports: <https://gitlab.com/hrbrmstr/securitytrails/issues>
+#' 
+#' @md
+#' @name securitytrails
+#' @docType package
+#' @author Bob Rudis (bob@@rud.is)
+#' @import httr
+#' @importFrom jsonlite fromJSON
+NULL
